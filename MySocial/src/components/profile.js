64fa57b235db025c987eaf53795ecab8e0b904e2
KaissSarragossa <2466568@cegepmv.ca>
2025-03-05 21:12:09 -0500

    ajout des tout les props

--- MySocial/src/components/profile.js
+++ MySocial/src/components/profile.js
@@ -1,9 +1,11 @@
+import cards from "./cards";
+
 const profile = [
     {
         profileimage : "https://images.unsplash.com/photo-1513721032312-6a18a42c8763?w=152&h=152&fit=crop&crop=faces",
         nom : "Jane Doe",
         bio : "Lorem ipsum dolor sit amet, consectetur adipiscing elit. 📷✈️🏕️",
-        post : "4",
+        post : cards.length,
         followers : "188",
         following : "206",
     }
